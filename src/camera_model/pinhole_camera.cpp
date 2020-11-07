a83d7aef98e0efbbcbb7e8dafa5fa492bedea86d
poifull10 <poifull10@gmail.com>
2020-11-08 02:11:01 +0900

    Unify floating type

--- src/camera_model/pinhole_camera.cpp
+++ src/camera_model/pinhole_camera.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <stdexcept>
 
+#include "../configuration/constant.h"
 #include "../util/cv_primitive.h"
 
 namespace tsfm {
 
 namespace {
+
 cv::Mat constructK(const PinholeCamera::IntrincsicParameter& intr) {
-  cv::Mat mat = cv::Mat::eye(3, 3, CV_64F);
+  cv::Mat mat = CvMatMaker<TSFMFloatType>::makeEye(3, 3);
   mat.at<double>(0, 0) = intr.fx;
   mat.at<double>(0, 2) = intr.cx;
   mat.at<double>(1, 1) = intr.fy;
@@ -17,6 +19,7 @@ cv::Mat constructK(const PinholeCamera::IntrincsicParameter& intr) {
   return mat;
 }
 }  // namespace
+
 void PinholeCamera::setIntrinsic(const std::unordered_map<std::string, double>& param) {
   if (param.count("fx") == 0) {
     throw std::invalid_argument("param needs fx");
@@ -40,18 +43,20 @@ Vec2 PinholeCamera::project(const Vec3& p) const {
   const cv::Mat K = constructK(intrinsic_);
   const cv::Mat vP = vec3ToCvMat(p);
   const cv::Mat projected = K * vP;
-  return {projected.at<double>(0, 0) / projected.at<double>(2, 0), projected.at<double>(1, 0) / projected.at<double>(2, 0)};
+  const Vec2 projectedVec{projected.at<double>(0, 0), projected.at<double>(1, 0)};
+  return projectedVec / projected.at<double>(2, 0);
 }
 
 Vec3 PinholeCamera::unproject(const Vec2& p) const {
   const cv::Mat K = constructK(intrinsic_);
   const cv::Mat vP = vec2ToCvMat(p, true);
   const cv::Mat unprojected = K.inv() * vP;
-  return {unprojected.at<double>(0, 0) / unprojected.at<double>(2, 0), unprojected.at<double>(1, 0) / unprojected.at<double>(2, 0), 1.0F};
+  const Vec3 unprojectedVec{unprojected.at<double>(0, 0), unprojected.at<double>(1, 0), unprojected.at<double>(2, 0)};
+  return unprojectedVec / unprojectedVec[2];
 }
 
 cv::Mat PinholeCamera::K() const {
-  cv::Mat K = cv::Mat::eye(3, 3, CV_64F);
+  cv::Mat K = CvMatMaker<TSFMFloatType>::makeEye(3, 3);
   K.at<double>(0, 0) = intrinsic_.fx;
   K.at<double>(1, 1) = intrinsic_.fy;
   K.at<double>(0, 2) = intrinsic_.cx;
